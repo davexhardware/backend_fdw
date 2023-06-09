1fcc54ec561b52fc647ebe9f8a7d20bdd0d95dd8
Davide <davexsoia@gmail.com>
2023-06-09 18:19:46 +0200

    0.1.1 made corrections

--- apis/register.js
+++ apis/register.js
@@ -1,10 +1,11 @@
 const users=require('../models/users');
 const lib=require('./lib');
 let register = (req,res)=>{
+    let email,password,firstname,lastname=[req.body['email'],req.body['password'],req.body['firstname'], req.body['lastname']]
     // require fields for registration checking:
-    if(req.body.email && req.body.password && req.body.firstname && req.body.lastname) {
+    if( email && password && firstname && lastname) {
         // required fields validation
-        if(lib.validateEmail(req.body.email) && lib.validateNonHashPassword(req.body.password) && lib.validateName(req.body.firstname) && lib.validateName(req.body.lastname)){
+        if(lib.validateEmail(email) && lib.validateHashPassword(password) && lib.validateName(firstname) && lib.validateName(lastname)){
 
         }
     }else res.json({error :"didn't receive all the required fields"})
