dc5107a3fddd0c996f6cc6ebdb8131e944d973d1
Davide <davexsoia@gmail.com>
2023-06-17 11:49:48 +0200

    0.4.1 add friend functionality completed

--- apis/friends.js
+++ apis/friends.js
@@ -1,10 +1,12 @@
 const users=require('../models/users');
 const lib = require("./lib");
+const {Types} = require("mongoose");
 let getfriends=function(req,res){
     lib.authenticateToken(req,res,()=> {
         let userid=req.user['id']
         users.findById(userid).
         then( (doc)=>{
+
             doc.populate('friends').then(
                 (docp)=>{
                     let friends=Array();
@@ -17,8 +19,17 @@ let getfriends=function(req,res){
 }
 let addfriend= (req,res)=>{
     lib.authenticateToken(req,res,()=> {
-        userid=re.user['id']
+        let userid=req.user['id']
+        users.findOne({email:req.body.email}).then( (doc)=> {
+            if(String(doc._id)===userid){
+                return res.status(401).json({error:"cannot add yourself as a friend"}).end()
+            }
+            Promise.all([users.updateOne({_id:userid},{ $push:{friends: doc._id}}),users.updateOne({_id:String(doc._id)},{$push:{friends:userid}})])
+            .then(ok=>{
+                res.status(200).send({ok:"friend added"})
+            }).catch(err=>{ res.status(500).json({error:'Pushing arrays error: '+err.message})})
 
+        }).catch(err=> {return res.status(404).json({error: "friend not registered "+err.message}) })
     })
 }
 module.exports={getfriends, addfriend}
